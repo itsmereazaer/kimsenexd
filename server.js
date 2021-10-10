49a3b423cf932261bb91a4c1fc75e599e0af6a46
itsmereazaer <75921515+itsmereazaer@users.noreply.github.com>
2021-10-10 20:15:30 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+import WebSocket from 'ws';
+import { scrapeProxys } from  "./modules/proxyManager.js"
+import { Client } from  "./modules/userModule.js"
+import config from './config.js';
+import * as fs from 'fs';
+
+console.log('Coded by Jiexyn');
+
+const wss = new WebSocket.Server({
+	port: 3523
+});
+
+function parsePT(pt) {
+    var type = pt.toLowerCase();
+    if (!['socks4', 'socks5', 'http'].includes(type))
+        type = 'socks4';
+    return type;
+}
+wss.on('listening', (ws) => {
+    scrapeProxys(config.useProxyApi, parsePT(config.proxyType));
+})
+wss.on('connection', (ws, req) => {
+	ws.Client = new Client(ws, req);
+});
+
+export function getConfig() {
+    return config;
+}
